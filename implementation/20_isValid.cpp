fbbf88300af1b66504a90acf78ffda0ad36f1426
Pomelo <jixingyou@sjtu.edu.cn>
2019-05-22 19:01:36 +0800

    Summary: complete task 20 to task 21
    1. parenthess validation: stack
    2. merge two sorted list

--- implementation/20_isValid.cpp
+++ implementation/20_isValid.cpp
@@ -2,23 +2,36 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        auto len = s.length();
-        for (auto i = 0; i < len; ++i) {
-        	if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-        		stk.push(s[i]);
-        	}
-        	if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
-        		if (stk.empty() == true) {
-        			return false;
-        		}
-        		auto ch = stk.top();
-        		if ((s[i] == ')' && ch == '(') || (s[i] == ']' && ch == '[') || (s[i] == '}' && ch == '{')) {
-        			stk.pop();
-        		} else {
-        			return false;
-        		}
-        	}
-        }
-        return stk.empty();
+        for (int i = 0; i < s.size(); ++i) {
+			if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+				stk.push(s[i]);
+			} else {
+				if (stk.empty()) {
+					return false;
+				}
+				switch (s[i])
+				{
+				case ')':
+					if (stk.top() != '(') {
+						return false;
+					}
+					break;
+				case ']':
+					if (stk.top() != '[') {
+						return false;
+					}
+					break;
+				case '}':
+					if (stk.top() != '{') {
+						return false;
+					}
+					break;
+				default:
+					break;
+				}
+				stk.pop();
+			}
+		}
+		return stk.empty();
     }
 };
--- implementation/21_mergeTwoLists.cpp
+++ implementation/21_mergeTwoLists.cpp
@@ -9,16 +9,19 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    	if (l1 == NULL) {
-    		return l2;
-    	}
-        if (l2 == NULL) {
-            return l1;
-        }
-    	if (l1->val > l2->val) {
-    		std::swap(l1, l2);
-    	}
-    	l1->next = mergeTwoLists(l1->next, l2);
-    	return l1;
+    	ListNode* dummy = new ListNode(-1);
+		ListNode* curr = dummy;
+		while (l1 && l2) {
+			if (l1->val < l2->val) {
+				curr->next = l1;
+				l1 = l1->next;
+			} else {
+				curr->next = l2;
+				l2 = l2->next;
+			}
+			curr = curr->next;
+		}
+		curr->next = l1 ? l1 : l2;
+		return dummy->next;
     }
 };
