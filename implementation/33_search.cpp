c43fd61d2024d7f5ac5cbc3afece24524c5f159b
Pomelo <jixingyou@sjtu.edu.cn>
2019-05-30 11:52:18 +0800

    Summary: complete 33
    compare nums[mid] with nums[right] --> binary search --> update left and right

--- implementation/33_search.cpp
+++ implementation/33_search.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            /*
+                [5　　6　　7　　 0　　1　　2　　4]
+            */
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[left] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
     }
 };
