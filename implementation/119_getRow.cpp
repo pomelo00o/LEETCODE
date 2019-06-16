ec8a2c13f0104e0374be110bd98337ba65abf256
Pomelo <jixingyou@sjtu.edu.cn>
2019-06-16 21:31:03 +0800

    trello T-1

--- implementation/119_getRow.cpp
+++ implementation/119_getRow.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> result(rowIndex + 1, 1);
-		for(int i = 1; i < result.size()-1; ++i) {
-			for(int j = rowIndex - 1; j >=i; --j) {
-				result[j] = result[j + 1] + result[j];
+		for(int i = 1; i < result.size() - 1; ++i) {
+			for(int j = rowIndex - 1; j >= i; --j) {
+				result[j] += result[j + 1];
 			}
 		}
 		return result;
