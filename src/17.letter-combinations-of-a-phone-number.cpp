144a420c26ee9f222b84b1891dd45120096c39cd
Tom-Pomelo <jixingyou@sjtu.edu.cn>
2019-09-22 15:11:49 -0700

    lc backtracking

--- src/17.letter-combinations-of-a-phone-number.cpp
+++ src/17.letter-combinations-of-a-phone-number.cpp
@@ -4,9 +4,31 @@
  * [17] Letter Combinations of a Phone Number
  */
 class Solution {
+private:
+    unordered_map<char, string> umap = 
+        {{'2', "abc"},  {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
 public:
     vector<string> letterCombinations(string digits) {
-        
+        if (digits.empty()) return {};
+        vector<string> combinations;
+        string curr;
+        backtrack(combinations, curr, digits);
+        return combinations;
+    }
+
+    void backtrack(vector<string>& combinations, string curr, string digits) {
+        int sz = curr.size();
+        if (sz == digits.size()) {
+            combinations.push_back(curr);
+            return;
+        }
+        char c = digits[sz];
+        string letters = umap[c];
+        for (int i = 0; i < letters.size(); ++i) {
+            curr.push_back(letters[i]);
+            backtrack(combinations, curr, digits);
+            curr.pop_back();
+        }
     }
 };
 
