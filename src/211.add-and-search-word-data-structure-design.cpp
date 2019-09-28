2b7df2054340e0f3415f8297649198e027acb9ba
Tom-Pomelo <jixingyou@sjtu.edu.cn>
2019-09-27 17:25:24 -0700

    lc fb

---
+++ src/211.add-and-search-word-data-structure-design.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=211 lang=cpp
+ *
+ * [211] Add and Search Word - Data structure design
+ */
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
